0031eacfd2a9e3aad8961ac70434d96ddde11f96
Kareem Aboelnaga <kaaboelnaga@gmail.com>
2025-05-23 21:51:20 +0300

    modified 3

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,11 +1,13 @@
 let args = process.argv.slice(2);
 let i = 0;
+let str = '';
 for (i in args);
 if (i === 0){
     console.log('No argument');
 }
 else{
     for (const fruit of args) {
-        console.log(fruit);
+        str += fruit + ' ';
     }
 }
+console.log(str.slice(0,-1));
