07ba8995ceb9a41d5c370492c82c2c9191a450fa
Kareem Aboelnaga <kaaboelnaga@gmail.com>
2025-05-23 22:16:54 +0300

    done 8

---
+++ 8-square.js
@@ -0,0 +1,10 @@
+let args = process.argv.slice(2);
+
+if(args.length === 0){
+    console.log('Missing size');
+}
+else{
+    for (let i = 0; i < args[0] ; i++){
+        console.log('X'.repeat(args[0]));
+    }
+}
